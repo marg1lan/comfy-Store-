86d81fda41cab74353fd39a9837a09221ac58f76
margilann <eshermatov07@gmail.com>
2024-05-08 01:55:24 +0300

    tugatildi

---
+++ src/components/ProductsContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductsGrid from '../components/ProductsGrid'
+
+function ProductsContainer() {
+  return (
+    <div>
+      <ProductsGrid></ProductsGrid>
+     </div>
+  )
+}
+
+export default ProductsContainer
