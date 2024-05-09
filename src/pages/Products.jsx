4c992a4dd687adb2eaf4c9e4dc8c45a05bd6b6cd
margilann <eshermatov07@gmail.com>
2024-05-10 00:34:49 +0300

    tugatildi

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,7 +6,10 @@ import Filters from '../components/Filters'
 const url = '/products'
 
   export const loader = async ({request}) => {
-    const response = await customFetch(url)
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries()
+    ])
+    const response = await customFetch(url , {params})
     const products = response.data.data
     const meta = response.data.meta
  
