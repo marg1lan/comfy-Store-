1f9e1c8c9f8a735585c050fbbeafc09c97a4d644
margi1an <elbekshermatov69@gmail.com>
2024-05-16 13:17:50 +0300

    uyga vazifa tugatildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,14 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Cart, Chekout,SingleProduct, HomeLayout, Landing, Login, Orders, Products, Register , Error, About } from "./pages";
-import { ErrorElement } from "./components";
-
-import { loader as LandingLoader } from "./pages/Landing";
-import {loader as SingleProductLoader} from './pages/SingleProduct'
-import {loader as ProductsLoader} from './pages/Products'
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Card from './Components/Card'
 
 function App() {
-  const routes = createBrowserRouter([
-    {
-      path: "/",
-      element: <HomeLayout />,
-      errorElement:<Error/>,
-      children: [
-        {
-          index: true,
-          element: <Landing />,
-          errorElement:<ErrorElement></ErrorElement>,
-          loader:LandingLoader  
-        },
-        {
-          path: "/products",
-          element: <Products />,
-          loader:ProductsLoader,
-        },
-        {
-          path: "/about",
-          element: <About />,
-        },
-        {
-          path: "/products/:id",
-          element: <SingleProduct/>,
-          loader:SingleProductLoader
-        },
-        {
-          path: "/cart",
-          element: <Cart />,
-        },
-        {
-          path: "/chekout",
-          element: <Chekout />,
-        },
-        {
-          path: "/orders",
-          element: <Orders />,
-        },
-      ],
-      
-    },
-    {
-    path:"/login",
-    errorElement:<Error/>,
-    element:<Login/>
-    },
-    {
-      path:"/register",
-      errorElement:<Error/>,
-      element:<Register/>
-      },
-
-  ])
-  return <RouterProvider router={routes} />;
+  return  (
+    <div className='align-content'>
+      <Navbar></Navbar>
+      <Card></Card>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/ErrorElement.jsx
+++
@@ -1,7 +0,0 @@
-import { useRouteError } from "react-router-dom";
-
-function ErrorElement() {
-  const error = useRouteError();
-  return <div className="font-bold text-5xl ">There was an error ...</div>;
-}
-export default ErrorElement;
